a67146959a409042c123bc384d8d097e330a0dcc
Tamires <tamires.ei04@gmail.com>
2025-05-05 11:59:47 -0300

    updated code to godot 4

--- scripts/demon.gd
+++ scripts/demon.gd
@@ -41,12 +41,22 @@ func _physics_process(delta):
 	# Simula visão com raycast manual (ou pode usar RayCast3D)
 	if distance <= detection_distance:
 		var space_state = get_world_3d().direct_space_state
-		var result = space_state.intersect_ray({
-			"from": global_position,
-			"to": player.global_position,
-			"exclude": [self],
-			"collision_mask": collision_mask
-		})
+		var ray_params = PhysicsRayQueryParameters3D.new()
+		ray_params.from = global_position
+		ray_params.to = player.global_position
+		ray_params.exclude = [self]
+		ray_params.collision_mask = collision_mask
+		
+		var result = space_state.intersect_ray(ray_params)
+		
+		
+		
+		#var result = space_state.intersect_ray({
+		#	"from": global_position,
+		#	"to": player.global_position,
+		#	"exclude": [self],
+		#	"collision_mask": collision_mask
+		#})
 
 		if result and result.collider == player:
 			if not is_chasing:
@@ -59,7 +69,7 @@ func _physics_process(delta):
 	if is_chasing:
 		agent.set_target_position(player.global_position)
 		agent.set_velocity(Vector3.ZERO) # pra forçar novo cálculo
-		agent.update()  # força atualização
+		#agent.update()  # força atualização
 		var next_path = agent.get_next_path_position()
 		var move_direction = (next_path - global_position).normalized()
 		velocity = move_direction * speed
